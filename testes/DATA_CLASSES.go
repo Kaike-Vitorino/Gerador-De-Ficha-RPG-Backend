07452d26a8611c2058bf4cd4615d9bf07e859be5
Kaike-Vitorino <kaikevoliveira@gmail.com>
2024-05-29 17:35:22 -0300

    V0.431 Backend COmpletando o commit anterior que foi faltando um arquivo

--- testes/DATA_CLASSES.go
+++ testes/DATA_CLASSES.go
@@ -1,20 +1,26 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
 // Classe representa as informacoes de uma classe
 type Classe struct {
-	AtributoChave string
-	Pericias      []string
-	Equipamentos  Equipamento
-	DadosRecurso  map[string]string
+	AtributoChave string            `json:"atributo_chave"`
+	Pericias      []string          `json:"pericias"`
+	Equipamentos  EquipamentoClasse `json:"equipamentos"`
+	DadosRecurso  map[string]string `json:"dados_recurso"`
 }
 
-// Equipamento representa os equipamentos de uma classe
-type Equipamento struct {
-	Arma            []string
-	Armadura        *string
-	Itens           int
-	ArtefatoMusical []string
-	Cavalo          int
+// EquipamentoClasse representa os equipamentos de uma classe
+type EquipamentoClasse struct {
+	Arma            []string `json:"arma"`
+	Armadura        *string  `json:"armadura"`
+	Itens           int      `json:"itens"`
+	ArtefatoMusical []string `json:"artefato_musical"`
+	Cavalo          int      `json:"cavalo"`
 }
 
 // PersonagemClasses armazena todas as classes e as infos dela
@@ -24,79 +30,44 @@ type PersonagemClasses struct {
 	TalentosClasses map[string][]string
 }
 
-// NewPersonagemClasses inicializa PersonagemClasses com valores padroes
-func NewPersonagemClasses(armas1M map[string]map[string]string) *PersonagemClasses {
-	armaduras1MLista := make([]string, 0, len(armas1M))
-	for k := range armas1M {
-		armaduras1MLista = append(armaduras1MLista, k)
+// Função para carregar dados de classes de um arquivo JSON
+func carregarClasses(filename string) (map[string]Classe, map[string][]string, error) {
+	var data struct {
+		Classes         map[string]Classe   `json:"Classes"`
+		TalentosClasses map[string][]string `json:"TalentosClasses"`
+	}
+	err := readJSON(filename, &data)
+	return data.Classes, data.TalentosClasses, err
+}
+
+// Função para ler dados JSON de um arquivo
+func readJSON(filename string, v interface{}) error {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, v)
+}
+
+// NewPersonagemClasses inicializa PersonagemClasses com valores carregados do arquivo JSON
+func NewPersonagemClasses(filename string) (*PersonagemClasses, error) {
+	if filename == "" {
+		filename = "data/classes.json"
+	}
+
+	classes, talentosClasses, err := carregarClasses(filename)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao carregar classes: %v", err)
+	}
+
+	classNames := make([]string, 0, len(classes))
+	for name := range classes {
+		classNames = append(classNames, name)
 	}
 
 	return &PersonagemClasses{
-		Classes: []string{"Caçador", "Druida", "Mago", "Rider", "Guerreiro", "Ladino", "Mascate", "Bardo"},
-		ClasseInfo: map[string]Classe{
-			"Caçador": {
-				AtributoChave: "Agilidade",
-				Pericias:      []string{"Furtividade", "Movimentação", "Pontaria", "Patrulha", "Sobrevivência"},
-				Equipamentos:  Equipamento{Arma: []string{"Arco", "Funda"}, Armadura: nil, Itens: 2, ArtefatoMusical: nil, Cavalo: 0},
-				DadosRecurso:  map[string]string{"Comida": "D8", "Água": "D8", "Flechas": "D10", "Prata": "D6"},
-			},
-			"Druida": {
-				AtributoChave: "Inteligência",
-				Pericias:      []string{"Resiliência", "Sobrevivência", "Discernimento", "Cura", "Adestramento"},
-				Equipamentos:  Equipamento{Arma: []string{"Bastão", "Faca"}, Armadura: nil, Itens: 1, ArtefatoMusical: nil, Cavalo: 0},
-				DadosRecurso:  map[string]string{"Comida": "D8", "Água": "D8", "Prata": "D6"},
-			},
-			"Mago": {
-				AtributoChave: "Inteligência",
-				Pericias:      []string{"Artesanato", "Artimanha", "Tradição", "Discernimento", "Manipulação"},
-				Equipamentos:  Equipamento{Arma: []string{"Bastão", "Faca"}, Armadura: nil, Itens: 1, ArtefatoMusical: nil, Cavalo: 0},
-				DadosRecurso:  map[string]string{"Comida": "D6", "Água": "D8", "Prata": "D8"},
-			},
-			"Rider": {
-				AtributoChave: "Agilidade",
-				Pericias:      []string{"Resiliência", "Luta", "Pontaria", "Sobrevivência", "Adestramento"},
-				Equipamentos:  Equipamento{Arma: []string{"Lança Curta", "Machadinha", "Arco Curto", "Funda"}, Armadura: nil, Itens: 1, ArtefatoMusical: nil, Cavalo: 1},
-				DadosRecurso:  map[string]string{"Comida": "D8", "Água": "D8", "Flechas": "D10", "Prata": "D6"},
-			},
-			"Guerreiro": {
-				AtributoChave: "Força",
-				Pericias:      []string{"Potência", "Resiliência", "Luta", "Movimentação"},
-				Equipamentos:  Equipamento{Arma: armaduras1MLista, Armadura: StringPtr("Couro"), Itens: 1, ArtefatoMusical: nil, Cavalo: 0},
-				DadosRecurso:  map[string]string{"Comida": "D8", "Água": "D6", "Prata": "D6"},
-			},
-			"Ladino": {
-				AtributoChave: "Agilidade",
-				Pericias:      []string{"Luta", "Furtividade", "Artimanha", "Movimentação", "Manipulação"},
-				Equipamentos:  Equipamento{Arma: []string{"Adaga"}, Armadura: nil, Itens: 2, ArtefatoMusical: nil, Cavalo: 0},
-				DadosRecurso:  map[string]string{"Comida": "D6", "Água": "D6", "Prata": "D10"},
-			},
-			"Mascate": {
-				AtributoChave: "Empatia",
-				Pericias:      []string{"Artesanato", "Artimanha", "Patrulha", "Discernimento", "Manipulação"},
-				Equipamentos:  Equipamento{Arma: []string{"Faca"}, Armadura: nil, Itens: 3, ArtefatoMusical: nil, Cavalo: 0},
-				DadosRecurso:  map[string]string{"Comida": "D8", "Água": "D8", "Prata": "D12"},
-			},
-			"Bardo": {
-				AtributoChave: "Empatia",
-				Pericias:      []string{"Tradição", "Discernimento", "Manipulação", "Atuação", "Cura"},
-				Equipamentos:  Equipamento{Arma: []string{"Faca"}, Armadura: nil, Itens: 1, ArtefatoMusical: []string{"Lira", "Flauta"}, Cavalo: 0},
-				DadosRecurso:  map[string]string{"Comida": "D8", "Água": "D6", "Prata": "D8"},
-			},
-		},
-		TalentosClasses: map[string][]string{
-			"Caçador":   {"Caminho da Fera", "Caminho da Flecha", "Caminho da Floresta"},
-			"Druida":    {"Caminho da Cura", "Caminho da Visão", "Caminho do Metamorfo"},
-			"Mago":      {"Caminho da Morte", "Caminho das Rochas", "Caminho do Sangue", "Caminho dos Símbolos"},
-			"Rider":     {"Caminho das Planícies", "Caminho do Cavaleiro", "Caminho do Companheiro"},
-			"Guerreiro": {"Caminho da Lâmina", "Caminho do Escudo", "Caminho do Inimigo"},
-			"Ladino":    {"Caminho do Matador", "Caminho do Rosto", "Caminho do Veneno"},
-			"Mascate":   {"Caminho da Mentira", "Caminho das Muitas Coisas", "Caminho do Ouro"},
-			"Bardo":     {"Caminho da Canção", "Caminho do Grito de Guerra", "Caminho do Hino"},
-		},
-	}
-}
-
-// StringPtr retorna um ponteiro para o valor da string que a gente precise.(usado para armadura)
-func StringPtr(s string) *string {
-	return &s
+		Classes:         classNames,
+		ClasseInfo:      classes,
+		TalentosClasses: talentosClasses,
+	}, nil
 }
